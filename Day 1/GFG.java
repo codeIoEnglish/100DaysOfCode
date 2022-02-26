de825c2ce5a57a98c67682ebcdf9a142127a1005
jaswanthvaithiswaran <jaswanthvaithiswaran@gmail.com>
2022-02-26 10:04:26 +0530

    java code for upper and lower triangle sum

---
+++ Day 1/GFG.java
@@ -0,0 +1,58 @@
+    import java.io.*;
+    import java.util.*;
+
+   public class GFG
+    {
+        public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            
+            while(t-- > 0)
+            {
+                int n = sc.nextInt();
+                
+                int matrix[][] = new int[n][n];
+                for(int i = 0; i < n; i++)
+                    for(int j = 0; j < n; j++)
+                        matrix[i][j] = sc.nextInt();
+    
+                Solution ob = new Solution();
+                ArrayList<Integer> ans = ob.sumTriangles(matrix,n);
+                for (Integer val: ans) 
+                    System.out.print(val+" "); 
+                System.out.println();
+            }
+            sc.close();
+        }
+    }
+    
+    
+    class Solution
+    {
+        
+        static ArrayList<Integer> sumTriangles(int matrix[][], int n)
+        {
+            int uppersum =0;
+            int lowersum=0;
+
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(i<=j)
+                    {
+                        uppersum+=matrix[i][j];
+                    }
+                    if(i>=j)
+                    {
+                        lowersum+=matrix[i][j];
+                    }
+                }
+            }
+            ArrayList<Integer> answer = new ArrayList<>();
+            answer.add(uppersum);
+            answer.add(lowersum);
+            return answer;
+        }
+    }
